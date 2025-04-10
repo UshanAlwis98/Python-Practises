ac6bc741abd8f6c9a234527893870e55cea3e093
Ushan Alwis Github <104507543+UshanAlwis98@users.noreply.github.com>
2025-04-10 14:01:51 +0530

    Print Statement Added.

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-
-
-print("Hello World 01")
-print("Let's Cooked")
-print("Hello World I am 'Ushan'")
-print('My "name" is Loshitha')
+print('Hello World') # Single Quote
+print("Hello World") # Double Quote
+print('She Said: "I am hungry"') # Double Quote inside Single Quote
+print("She Said: 'I am hungry'") # Single Quote inside Double Quote
+print("She said: \"I am Hungry\"") # Before Quotes Black slash is required
