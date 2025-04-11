ff814503c023b7265d9bd19740c0c6e17e11aafd
Ushan Alwis Github <104507543+UshanAlwis98@users.noreply.github.com>
2025-04-11 20:48:16 +0530

    Len Function added.

--- main.py
+++ main.py
@@ -1,7 +1,25 @@
-print('Hello World') # Single Quote
-print("Hello World") # Double Quote
-print('She Said: "I am hungry"') # Double Quote inside Single Quote
-print("She Said: 'I am hungry'") # Single Quote inside Double Quote
-print("She said: \"I am Hungry\"") # Before Quotes Black slash is required
-print("Hello World Line one \nHello World Line Two\nHello World Line Three") # Line by Line Break
-print("Hello"+" "+"Ushan") #String Concatenation
+# Print Statement
+
+
+# print('Hello World') # Single Quote
+# print("Hello World") # Double Quote
+# print('She Said: "I am hungry"') # Double Quote inside Single Quote
+# print("She Said: 'I am hungry'") # Single Quote inside Double Quote
+# print("She said: \"I am Hungry\"") # Before Quotes Black slash is required
+# print("Hello World Line one \nHello World Line Two\nHello World Line Three") # Line by Line Break
+# print("Hello"+" "+"Ushan") #String Concatenation
+
+# Input Prompt Statement
+
+# input("What's Your Name?")
+#
+# print("Hello " + input("What's your name ?"))
+
+
+# Input len() Function
+myFirstNameLength = len(input())
+myLastNameLength = len(input())
+
+print(myFirstNameLength * myLastNameLength)
+
+
