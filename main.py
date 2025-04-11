3c7ce7534c83ad9fc95828d8246295f86428405a
Ushan Alwis Github <104507543+UshanAlwis98@users.noreply.github.com>
2025-04-11 18:00:51 +0530

    Line Break Added.

--- main.py
+++ main.py
@@ -4,3 +4,4 @@ print('She Said: "I am hungry"') # Double Quote inside Single Quote
 print("She Said: 'I am hungry'") # Single Quote inside Double Quote
 print("She said: \"I am Hungry\"") # Before Quotes Black slash is required
 print("Hello World Line one \nHello World Line Two\nHello World Line Three") # Line by Line Break
+print("Hello"+" "+"Ushan") #String Concatenation
