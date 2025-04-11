b9b403c17966902c7c4ce89466ed09a0c4405dd4
Ushan Alwis Github <104507543+UshanAlwis98@users.noreply.github.com>
2025-04-12 04:10:28 +0530

    Final Task done.

--- main.py
+++ main.py
@@ -40,15 +40,33 @@
 
 
 # Python Variables Challange 03
-alice_spent = 420
-bob_spent = 380
-charlie_spent = 300
+# alice_spent = 420
+# bob_spent = 380
+# charlie_spent = 300
+#
+# equal_share = (alice_spent + bob_spent + charlie_spent) / 3
+# print(equal_share)
+#
+# alice_balance = (alice_spent - equal_share)
+# bob_balance = (bob_spent - equal_share)
+# charlie_balance = (charlie_spent - equal_share)
 
-equal_share = (alice_spent + bob_spent + charlie_spent) / 3
-print(equal_share)
-
-alice_balance = (alice_spent - equal_share)
-bob_balance = (bob_spent - equal_share)
-charlie_balance = (charlie_spent - equal_share)
 
 
+# Day 01 Final Task
+
+#1. Create a greeting for your program.
+print("Click Run to run the final project you will build.")
+
+#2. Ask the user for the city that they grew up in.
+home_Town = input("What's the name of the city you grew up in?\n")
+
+#3. Ask the user for the name of a pet.
+pet_Name = input("What's your pet's name?\n")
+
+#4. Combine the name of their city and pet and show them their band name.
+print(home_Town + " " + pet_Name)
+
+#5. Make sure the input cursor shows on a new line:
+
+# Solution: https://replit.com/@appbrewery/band-name-generator-end
