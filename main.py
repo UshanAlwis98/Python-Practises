4246ad7b1ca193cd09dec8c1b6144e7d2274b662
Ushan Alwis Github <104507543+UshanAlwis98@users.noreply.github.com>
2025-04-11 05:42:33 +0530

    Line Break Added.

--- main.py
+++ main.py
@@ -3,3 +3,4 @@ print("Hello World") # Double Quote
 print('She Said: "I am hungry"') # Double Quote inside Single Quote
 print("She Said: 'I am hungry'") # Single Quote inside Double Quote
 print("She said: \"I am Hungry\"") # Before Quotes Black slash is required
+print("Hello World Line one \nHello World Line Two\nHello World Line Three") # Line by Line Break
