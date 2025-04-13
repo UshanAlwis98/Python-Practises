fe60fcdc6ee11f2deea4ccd1de474d9a1e0185cd
Ushan Alwis Github <104507543+UshanAlwis98@users.noreply.github.com>
2025-04-13 07:53:25 +0530

    Type Casting Learned.

--- main_day02.py
+++ main_day02.py
@@ -1,2 +1,14 @@
-# Subscripting
-print("Hello"[0])
+# Subscripting Example 01
+# print("Hello"[0])
+
+# Subscripting Example 02
+# my_First_Word = "Hello"
+# my_Second_Word = "Vowel"
+# my_Inputted_Word = input("Give me your idea\n")
+# my_New_Word = print(my_First_Word[1]+my_Second_Word[2]+my_Inputted_Word[3])
+
+# Type Casting Example
+
+num_Char_Name = len(input("What's Your Name?\n"))
+new_num_Char_Name = str(num_Char_Name)
+print("My name has " + new_num_Char_Name + " Characters.")
