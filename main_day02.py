5366c47bd44561907111f07c4e139961e21dc6ba
Ushan Alwis Github <104507543+UshanAlwis98@users.noreply.github.com>
2025-04-13 08:02:20 +0530

    Type Casting Learned.

--- main_day02.py
+++ main_day02.py
@@ -7,8 +7,13 @@
 # my_Inputted_Word = input("Give me your idea\n")
 # my_New_Word = print(my_First_Word[1]+my_Second_Word[2]+my_Inputted_Word[3])
 
-# Type Casting Example
+# Type Casting Example 01
+# num_Char_Name = len(input("What's Your Name?\n"))
+# new_num_Char_Name = str(num_Char_Name)
+# print("My name has " + new_num_Char_Name + " Characters.")
 
-num_Char_Name = len(input("What's Your Name?\n"))
-new_num_Char_Name = str(num_Char_Name)
-print("My name has " + new_num_Char_Name + " Characters.")
+# Type Casting Example 02 - Add Integer and casted Float
+# print(100 + float("70.5"))
+
+# Type Casting Example 03 - Add Casted two Strings
+print(str(70)+str(3221))
