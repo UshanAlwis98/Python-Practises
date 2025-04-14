e262cb131cb6759986c8865779e48b5460cc7c3c
Ushan Alwis Github <104507543+UshanAlwis98@users.noreply.github.com>
2025-04-15 03:43:20 +0530

    Nested if Learned.

--- main_day03.py
+++ main_day03.py
@@ -1,7 +1,23 @@
 # Control Flow
-water_level = 90
+# water_level = 90
+#
+# if water_level>80:
+#     print("Water Level is high")
+# else:
+#     print("Water level is low")
 
-if water_level>80:
-    print("Water Level is high")
+
+# Nested If Statements
+age = int(input("Enter your age\n"))
+
+if age>5:
+    if age<12:
+        print("Your ticket price is $7")
+    elif age<18:
+        print("Your ticket price is $12")
+    elif age<22:
+        print("Your ticket price is $25")
+    else:
+        print("You are an adult")
 else:
-    print("Water level is low")
+    print("You can not ride the RollerCoaster")
