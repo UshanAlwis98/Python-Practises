a18a9ef23723d93d69397847a99c1aaea4349456
Ushan Alwis Github <104507543+UshanAlwis98@users.noreply.github.com>
2025-04-18 05:35:48 +0530

    Nested if Learned.

--- main_day03.py
+++ main_day03.py
@@ -8,16 +8,17 @@
 
 
 # Nested If Statements
-age = int(input("Enter your age\n"))
+# age = int(input("Enter your age\n"))
+#
+# if age>5:
+#     if age<12:
+#         print("Your ticket price is $7")
+#     elif age<18:
+#         print("Your ticket price is $12")
+#     elif age<22:
+#         print("Your ticket price is $25")
+#     else:
+#         print("You are an adult")
+# else:
+#     print("You can not ride the RollerCoaster")
 
-if age>5:
-    if age<12:
-        print("Your ticket price is $7")
-    elif age<18:
-        print("Your ticket price is $12")
-    elif age<22:
-        print("Your ticket price is $25")
-    else:
-        print("You are an adult")
-else:
-    print("You can not ride the RollerCoaster")
