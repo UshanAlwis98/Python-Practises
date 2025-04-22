5a06ef7754d1106af92b6c1d180488b4dc8a1d1f
Ushan Alwis Github <104507543+UshanAlwis98@users.noreply.github.com>
2025-04-23 02:13:20 +0530

    Logical Operator Challange beginning.

--- main_day03.py
+++ main_day03.py
@@ -107,30 +107,32 @@
 
 
 # Second Answer
-print("Thank you for choosing Python Pizza Deliveries!")
-size = input()  # What size pizza do you want? S, M, or L
-add_pepperoni = input()  # Do you want pepperoni? Y or N
-extra_cheese = input()  # Do you want extra cheese? Y or N
-# 🚨 Don't change the code above 👆
-# Write your code below this line 👇
-Bill =0
-if size == "S":
-   Bill += 15
-elif size == "M":
-    Bill += 20
-else:
-    Bill += 25
+# print("Thank you for choosing Python Pizza Deliveries!")
+# size = input()  # What size pizza do you want? S, M, or L
+# add_pepperoni = input()  # Do you want pepperoni? Y or N
+# extra_cheese = input()  # Do you want extra cheese? Y or N
+# # 🚨 Don't change the code above 👆
+# # Write your code below this line 👇
+# Bill =0
+# if size == "S":
+#    Bill += 15
+# elif size == "M":
+#     Bill += 20
+# else:
+#     Bill += 25
+#
+# if add_pepperoni == "Y":
+#     if size == "S":
+#         Bill += 2
+#     else:
+#         Bill += 3
+#
+# if extra_cheese == "Y" :
+#     Bill +=1
+#
+# print(f"Your final bill is:{Bill}")
 
-if add_pepperoni == "Y":
-    if size == "S":
-        Bill += 2
-    else:
-        Bill += 3
-
-if extra_cheese == "Y" :
-    Bill +=1
-
-print(f"Your final bill is:{Bill}")
+# Logical Operator Challange beginning
 
 
 
