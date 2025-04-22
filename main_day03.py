fa96e5db962bc0d8509562740766b584a898f697
Ushan Alwis Github <104507543+UshanAlwis98@users.noreply.github.com>
2025-04-22 20:04:58 +0530

    Pizza Challange Started.

--- main_day03.py
+++ main_day03.py
@@ -49,3 +49,15 @@
 #         print("Wrong digit. Access denied!")
 # else:
 #     print("Wrong digit. Access denied!")
+
+
+# Pizza Challange
+
+print("Thank you for choosing Python Pizza Deliveries!")
+size = input()  # What size pizza do you want? S, M, or L
+add_pepperoni = input()  # Do you want pepperoni? Y or N
+extra_cheese = input()  # Do you want extra cheese? Y or N
+# 🚨 Don't change the code above 👆
+# Write your code below this line 👇
+price_of_small_pizza = 15
+
