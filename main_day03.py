9ad2f239deb80b05dc555789bc960cea92588952
Ushan Alwis Github <104507543+UshanAlwis98@users.noreply.github.com>
2025-04-24 03:18:21 +0530

    Day 3 Final Project Finished.

--- main_day03.py
+++ main_day03.py
@@ -166,15 +166,56 @@
 
 
 # Final Project of Day 03
+print(r'''
+*******************************************************************************
+          |                   |                  |                     |
+ _________|________________.=""_;=.______________|_____________________|_______
+|                   |  ,-"_,=""     `"=.|                  |
+|___________________|__"=._o`"-._        `"=.______________|___________________
+          |                `"=._o`"=._      _`"=._                     |
+ _________|_____________________:=._o "=._."_.-="'"=.__________________|_______
+|                   |    __.--" , ; `"=._o." ,-"""-._ ".   |
+|___________________|_._"  ,. .` ` `` ,  `"-._"-._   ". '__|___________________
+          |           |o`"=._` , "` `; .". ,  "-._"-._; ;              |
+ _________|___________| ;`-.o`"=._; ." ` '`."\` . "-._ /_______________|_______
+|                   | |o;    `"-.o`"=._``  '` " ,__.--o;   |
+|___________________|_| ;     (#) `-.o `"=.`_.--"_o.-; ;___|___________________
+____/______/______/___|o;._    "      `".o|o_.--"    ;o;____/______/______/____
+/______/______/______/_"=._o--._        ; | ;        ; ;/______/______/______/_
+____/______/______/______/__"=._o--._   ;o|o;     _._;o;____/______/______/____
+/______/______/______/______/____"=._o._; | ;_.--"o.--"_/______/______/______/_
+____/______/______/______/______/_____"=.o|o_.--""___/______/______/______/____
+/______/______/______/______/______/______/______/______/______/______/_____ /
+*******************************************************************************
+''')
+
+print("Welcome to Treasure Island.")
+print("Your mission is to find the treasure.")
 
 
 
+direction = input("You're at a cross road.Where do u want to go ? Type left or right\n")
 
 
 
-
-
-
+if direction == "left":
+    swim_wait = input(
+        "You come to a lake.There is an island in the middle of the lake. Type 'wait' to wait for a boat.Type 'swim' to swim across \n")
+    if swim_wait == "wait":
+        door = input(
+            "You arrived at the island unharmed. There is a house with 3 doors.one red , one yellow and one blue. Which color do you choose ?\n")
+        if door == "blue":
+            print("Eaten by beasts. Game Over.")
+        elif door == "Red":
+            print("Burned by fire.Game Over.")
+        elif door == "yellow":
+            print("You Win")
+        else:
+            print("Game Over.")
+    else:
+        print("Attacked by trout.Game Over.")
+else:
+    print("Fall into a hole.Game Over.")
 
 
 
