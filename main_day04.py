2432c5357bbaca91e9a3cdfbceb10079d32956d5
Ushan Alwis Github <104507543+UshanAlwis98@users.noreply.github.com>
2025-04-26 02:59:58 +0530

    Rand Float learned.

--- main_day04.py
+++ main_day04.py
@@ -1,5 +1,8 @@
 # Random Numbers
 import random
 import  my_Module_day04
-# print(random.randint(100 ,102))
+# print(random.randint(100 ,102)) randint will return integer between 100 and 102
 print(my_Module_day04.pi_value)
+
+random_float = random.random()  #random.random() will give floating point number between 0.0 and 1.1
+print(random_float)
