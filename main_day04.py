836e7307f2496e6162c4ae6c4a6f7d7b0054f243
Ushan Alwis Github <104507543+UshanAlwis98@users.noreply.github.com>
2025-04-27 11:11:47 +0530

    Random integer generated.

--- main_day04.py
+++ main_day04.py
@@ -1,8 +1,8 @@
 # Random Numbers
 import random
 import  my_Module_day04
-# print(random.randint(100 ,102)) randint will return integer between 100 and 102
+print(random.randint(100 ,200)) #randint will return integer between 100 and 102
 print(my_Module_day04.pi_value)
 
-random_float = random.random()  #random.random() will give floating point number between 0.0 and 1.1
+random_float = random.random()  #random.random() will give floating point number between 0.0 and 1.0 not including 1
 print(random_float)
