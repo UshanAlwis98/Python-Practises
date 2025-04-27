f091cb949fcd2660b4bff4ad63066293c274c0d6
Ushan Alwis Github <104507543+UshanAlwis98@users.noreply.github.com>
2025-04-27 21:22:26 +0530

    Python list implemented.

--- main_day04.py
+++ main_day04.py
@@ -6,17 +6,21 @@ import  my_Module_day04
 
 random_float = random.random()  #random.random() will give floating point number between 0.0 and 1.0 not including 1
 
-print(random_float * 5)
+# print(random_float * 5)
+#
+# # Head and Tail Challenge
+# # Write your code below this line 👇
+# import random
+#
+# random_num = random.randint(0,1)
+# print(random_num)
+#
+# if random_num == 1:
+#   print("Heads")
+# else:
+#   print("Tails")
+# # Hint: Remember to import the random module first. 🎲
 
-# Head and Tail Challenge
-# Write your code below this line 👇
-import random
+# Python list data structure
 
-random_num = random.randint(0,1)
-print(random_num)
-
-if random_num == 1:
-  print("Heads")
-else:
-  print("Tails")
-# Hint: Remember to import the random module first. 🎲
+states_of_america = ["Delaware", "Pennsylvania"]
