c00c5b4ad4e79e9a7eca6418c564d89d01d22511
Ushan Alwis Github <104507543+UshanAlwis98@users.noreply.github.com>
2025-04-27 21:05:11 +0530

    Head and Tail Challenge Implemented.

--- main_day04.py
+++ main_day04.py
@@ -1,8 +1,22 @@
 # Random Numbers
 import random
 import  my_Module_day04
-print(random.randint(100 ,200)) #randint will return integer between 100 and 102
-print(my_Module_day04.pi_value)
+# print(random.randint(100 ,200)) #randint will return integer between 100 and 102
+# print(my_Module_day04.pi_value)
 
 random_float = random.random()  #random.random() will give floating point number between 0.0 and 1.0 not including 1
-print(random_float)
+
+print(random_float * 5)
+
+# Head and Tail Challenge
+# Write your code below this line 👇
+import random
+
+random_num = random.randint(0,1)
+print(random_num)
+
+if random_num == 1:
+  print("Heads")
+else:
+  print("Tails")
+# Hint: Remember to import the random module first. 🎲
