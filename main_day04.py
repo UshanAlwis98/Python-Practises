42132a7032659a410e266e9e679312b739b52864
Ushan Alwis Github <104507543+UshanAlwis98@users.noreply.github.com>
2025-04-28 00:35:54 +0530

    Python Randomization and List Challange Accepted.

--- main_day04.py
+++ main_day04.py
@@ -23,16 +23,22 @@ random_float = random.random()  #random.random() will give floating point number
 
 # Python list data structure
 
-states_of_america = [
-    "Alabama", "Alaska", "Arizona", "Arkansas", "California", "Colorado", "Connecticut",
-    "Delaware", "Florida", "Georgia", "Hawaii", "Idaho", "Illinois", "Indiana", "Iowa",
-    "Kansas", "Kentucky", "Louisiana", "Maine", "Maryland", "Massachusetts", "Michigan",
-    "Minnesota", "Mississippi", "Missouri", "Montana", "Nebraska", "Nevada", "New Hampshire",
-    "New Jersey", "New Mexico", "New York", "North Carolina", "North Dakota", "Ohio",
-    "Oklahoma", "Oregon", "Pennsylvania", "Rhode Island", "South Carolina", "South Dakota",
-    "Tennessee", "Texas", "Utah", "Vermont", "Virginia", "Washington", "West Virginia",
-    "Wisconsin", "Wyoming"
-]
+# states_of_america = [
+#     "Alabama", "Alaska", "Arizona", "Arkansas", "California", "Colorado", "Connecticut",
+#     "Delaware", "Florida", "Georgia", "Hawaii", "Idaho", "Illinois", "Indiana", "Iowa",
+#     "Kansas", "Kentucky", "Louisiana", "Maine", "Maryland", "Massachusetts", "Michigan",
+#     "Minnesota", "Mississippi", "Missouri", "Montana", "Nebraska", "Nevada", "New Hampshire",
+#     "New Jersey", "New Mexico", "New York", "North Carolina", "North Dakota", "Ohio",
+#     "Oklahoma", "Oregon", "Pennsylvania", "Rhode Island", "South Carolina", "South Dakota",
+#     "Tennessee", "Texas", "Utah", "Vermont", "Virginia", "Washington", "West Virginia",
+#     "Wisconsin", "Wyoming"
+# ]
+#
+#
+# print(states_of_america[-50])
 
+# Python Names Challenge with List,Randomization
 
-print(states_of_america[-50])
+my_names_list = input("Enter names\n")
+
+len(my_names_list)
