3391430fdb488ebb0cf6488175ea3cd5d6bdbbbb
Ushan Alwis Github <104507543+UshanAlwis98@users.noreply.github.com>
2025-05-23 00:02:27 +0530

    Python for loop and indentation

--- main_day05.py
+++ main_day05.py
@@ -1,4 +1,7 @@
 fruits = ["Apple", "Woodapple", "Pineapple", "Peach", "Pears"]
 
+# indentation is very important in this kind of for loops
 for fruitOneByOne in fruits:
     print( fruitOneByOne)
+    print(fruitOneByOne + " Pie")
+    print(fruits)
