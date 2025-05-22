8b5343a19b25605ae8873673153d15c12e1fa82b
Ushan Alwis Github <104507543+UshanAlwis98@users.noreply.github.com>
2025-05-23 00:08:57 +0530

    Student Height Challenge Accepted.

--- main_day05.py
+++ main_day05.py
@@ -1,7 +1,16 @@
-fruits = ["Apple", "Woodapple", "Pineapple", "Peach", "Pears"]
+# fruits = ["Apple", "Woodapple", "Pineapple", "Peach", "Pears"]
+#
+# # indentation is very important in this kind of for loops
+# for fruitOneByOne in fruits:
+#     print( fruitOneByOne)
+#     print(fruitOneByOne + " Pie")
+#     print(fruits)
 
-# indentation is very important in this kind of for loops
-for fruitOneByOne in fruits:
-    print( fruitOneByOne)
-    print(fruitOneByOne + " Pie")
-    print(fruits)
+
+# Input a Python list of student heights
+student_heights = input().split()
+for n in range(0, len(student_heights)):
+    student_heights[n] = int(student_heights[n])
+# 🚨 Don't change the code above 👆
+
+# Write your code below this row 👇
