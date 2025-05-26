0372dfe3aeb9769c3478b8aa583acee1002d77d1
Ushan Alwis Github <104507543+UshanAlwis98@users.noreply.github.com>
2025-05-26 13:51:30 +0530

    Student Average height challenge restarted.

--- main_day05.py
+++ main_day05.py
@@ -15,17 +15,3 @@ for n in range(0, len(student_heights)):
 # 🚨 Don't change the code above 👆
 
 # Write your code below this row 👇
-total_height = 0
-for height in student_heights:
-    total_height += height
-
-
-no_of_students = 0
-for student in student_heights:
-    no_of_students += 1
-
-average_height = round(total_height / no_of_students)
-
-print(f"total height = {total_height}")
-print(f"number of students = {no_of_students}")
-print(f"average height = {average_height}")
